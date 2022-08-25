5aef6aaa1f7398997fdddb27b777a386868045cf
Ameer Nasr <anasr@lab1r1s5.42abudhabi.ae>
2022-08-25 15:22:52 +0400

    Add tests for ft::map's operations functions
    Added tests for find(), count(), lower_bound(), upper_bound(),
    equal_range(), relational operators, and std::swap().

--- tests/map_tests.cpp
+++ tests/map_tests.cpp
@@ -6,7 +6,7 @@
 /*   By: anasr <anasr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 14:52:38 by ann               #+#    #+#             */
-/*   Updated: 2022/08/25 12:38:13 by anasr            ###   ########.fr       */
+/*   Updated: 2022/08/25 15:22:13 by anasr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -358,29 +358,126 @@ void	test_accuracy()
 	{
 		/*Observers*/
 		std::cout << "********** Testing Observers: **********" << std::endl;
+
+		/*key_comp*/
+		std::cout << "# key_comp() #" << std::endl;
+		
+		ft::map<float, char> mp;
+		ft::map<float, char>::key_compare keyCompReplica = mp.key_comp();
+		mp[1.1] = 'A';	mp[2.2] = 'B';	mp[3.3] = 'C';
+		mp[4.4] = 'D';	mp[5.5] = 'E';	mp[6.6] = 'G';
+		mp[7.7] = 'F';	mp[8.8] = 'K';	mp[9.9] = 'L';
+		
+		ft::map<float, char>::iterator it = mp.begin();
+		for (; keyCompReplica(it->first, mp.rbegin()->first); ++it)
+			std::cout << it->first << "->" << it->second << " - " << std::endl;			
+		
+		/*value_comp*/
+		std::cout << "# value_comp() #" << std::endl;
+		it = mp.begin();
+		for (it = mp.begin(); mp.value_comp()(*it, *mp.rbegin()); ++it)
+			std::cout << it->first << "->" << it->second << " - " << std::endl;
+	}
+	{
+		/*Operations*/
+		std::cout << "********** Testing Operations: **********" << std::endl;
 		{
-			/*key_comp*/
-			std::cout << "# key_comp() #" << std::endl;
+			/*find*/
+			std::cout << "# find() #" << std::endl;
+			std::list< ft::pair<int, std::string> > myNameIsListy;
+			for (int i = 0; i < 10000; ++i)
+				myNameIsListy.push_back(ft::make_pair(i, ::to_string(i)));
+		
+			ft::map<int, std::string> mp(myNameIsListy.begin(), myNameIsListy.end());
+			const ft::map<int, std::string> mpConst(myNameIsListy.begin(), myNameIsListy.end());
 			
-			ft::map<float, char> mp;
-			ft::map<float, char>::key_compare keyCompReplica = mp.key_comp();
-			mp[1.1] = 'A';	mp[2.2] = 'B';	mp[3.3] = 'C';
-			mp[4.4] = 'D';	mp[5.5] = 'E';	mp[6.6] = 'G';
-			mp[7.7] = 'F';	mp[8.8] = 'K';	mp[9.9] = 'L';
-			
-			ft::map<float, char>::iterator it = mp.begin();
-			for (; keyCompReplica(it->first, mp.rbegin()->first); ++it)
-				std::cout << it->first << "->" << it->second << " - " << std::endl;			
-			
-			/*value_comp*/
-			std::cout << "# value_comp() #" << std::endl;
-			
-			it = mp.begin();
-			for (it = mp.begin(); mp.value_comp()(*it, *mp.rbegin()); ++it)
-				std::cout << it->first << "->" << it->second << " - " << std::endl;
+			ft::map<int, std::string>::key_type	findMe = 0;
+			// non-const
+			for (findMe = -100; findMe < 1100; findMe += 50)
+				if (mp.find(findMe) != mp.end()) std::cout << mp.find(findMe)->second << std::endl;
+			// const
+			for (findMe = -100; findMe < 1100; findMe += 50)
+				if (mpConst.find(findMe) != mpConst.end()) std::cout << mpConst.find(findMe)->second << std::endl;	
+
+			/*count*/
+			std::cout << "# count() #" << std::endl;
+			for (findMe = -100; findMe < 1100; findMe += 30)
+				std::cout << mpConst.count(findMe) << " - ";
+			std::cout << std::endl;
+
+			/*upper_bound*/
+			std::cout << "# upper_bound() #" << std::endl;
+			for (findMe = -100; findMe < 1100; findMe += 10)
+				if (mp.upper_bound(findMe) != mp.end()) std::cout << mp.upper_bound(findMe)->second << std::endl;
+			for (findMe = -100; findMe < 1100; findMe += 10)
+				if (mpConst.upper_bound(findMe) != mpConst.end()) std::cout << mpConst.upper_bound(findMe)->second << std::endl;	
+			/*lower_bound*/
+			std::cout << "# lower_bound() #" << std::endl;
+			for (findMe = -100; findMe < 1100; findMe += 10)
+				if (mp.lower_bound(findMe) != mp.end()) std::cout << mp.lower_bound(findMe)->second << std::endl;
+			for (findMe = -100; findMe < 1100; findMe += 10)
+				if (mpConst.lower_bound(findMe) != mpConst.end()) std::cout << mpConst.lower_bound(findMe)->second << std::endl;
+
+			/*equal_range*/
+			std::cout << "# equal_range() #" << std::endl;
+			for (findMe = -100; findMe < 1100; findMe += 10)
+			{
+				ft::pair< ft::map<int, std::string>::iterator, ft::map<int, std::string>::iterator > checkMe;
+				checkMe = mp.equal_range(findMe);
+				if (checkMe.first != mp.end())
+					std::cout << checkMe.first->first << "->" << checkMe.first->second << " - ";
+				if (checkMe.second != mp.end())
+					std::cout << checkMe.second->first << "->" << checkMe.second->second << std::endl;
+				ft::pair< ft::map<int, std::string>::const_iterator, ft::map<int, std::string>::const_iterator > checkMeConst;
+				checkMeConst = mp.equal_range(findMe);
+				if (checkMeConst.first != mp.end())
+					std::cout << checkMeConst.first->first << "->" << checkMeConst.first->second << " - ";
+				if (checkMeConst.second != mp.end())
+					std::cout << checkMeConst.second->first << "->" << checkMeConst.second->second << std::endl;
+			}
+			// {
+			// 	/*get_allocator*/
+			// 	std::cout << "# get_allocator() #" << std::endl;
+			// 	ft::map<int, std::string> mp;
+
+			// 	ft::map<int, std::string>::value_type *ptr = mp.get_allocator().allocate(1000);
+
+			// 	for (int i = 0; i < 1000; ++i) mp.get_allocator().construct(ptr + i, ft::make_pair(i, "HELLO!"));
+
+			// 	for (int i = 0; i < 1000; ++i) std::cout << ptr[i] << " - ";
+			// 	std::cout << std::endl;
+
+			// 	for (int i = 0; i < 1000; ++i) mp.get_allocator().destroy(ptr + i);
+			// 	mp.get_allocator().deallocate(ptr, 1000);
+			// }
 		}
 		{
+			/*relational operators*/
+			std::cout << "# relational operators #" << std::endl;
+			std::list< ft::pair<int, std::string> > myNameIsListy;
+			for (int i = 0; i < 10000; ++i)
+				myNameIsListy.push_back(ft::make_pair(i, ::to_string(i)));
+		
+			ft::map<int, std::string> mpA(myNameIsListy.begin(), myNameIsListy.end());
+
+			myNameIsListy.clear();
+			for (int i = 0; i < 454; ++i)
+				myNameIsListy.push_back(ft::make_pair(i, ::to_string(i)));
+			ft::map<int, std::string> mpB(myNameIsListy.begin(), myNameIsListy.end());
+	
+			std::cout << (mpA == mpB) << std::endl;
+			std::cout << (mpA != mpB) << std::endl;
+			std::cout << (mpA < mpB) << std::endl;
+			std::cout << (mpA <= mpB) << std::endl;
+			std::cout << (mpA > mpB) << std::endl;
+			std::cout << (mpA >= mpB) << std::endl;
 			
+			/*std::swap specialization*/
+			std::cout << "# std::swap specialization #" << std::endl;
+
+			std::swap(mpA, mpB);
+			printMapInfo(mpA);
+			printMapInfo(mpB);
 		}
 	}
 	
