2b2cb60749a749d3ea0b44a2846ad10fe1197d2e
ann <anasr@student.42abudhabi.ae>
2022-08-30 07:42:32 +0400

    Fix small mistake in a map test file

--- tests/map/swap_clear.cpp
+++ tests/map/swap_clear.cpp
@@ -9,7 +9,7 @@ int main()
 		mpA.insert(ft::make_pair(i, 'A'));
 	ft::map<int, char> mpB;
 	for (int i = 0; i < 464; ++i)
-		mpA.insert(ft::make_pair(i, 'B'));
+		mpB.insert(ft::make_pair(i, 'B'));
 	ft::map<int, char>::iterator OldItA = mpA.begin();
 	ft::map<int, char>::iterator OldItB = mpB.begin();
 	
