0f974b044e080dc8a93386ee4e7a445ac971d7b3
ann <anasr@student.42abudhabi.ae>
2022-08-31 23:31:41 +0400

    Add more tests in vector's crazy.hpp

--- tests/vector/crazy.cpp
+++ tests/vector/crazy.cpp
@@ -14,9 +14,34 @@ int main()
 		std::cout << it->front() << "\t";
 	std::cout << std::endl;
 
+	vec.insert(vec.begin() + 1, ft::vector<int>(1, 1337));
+	vec.insert(vec.begin() + 3, ft::vector<int>(1, 1337));
+	vec.insert(vec.begin() + 5, ft::vector<int>(1, 1337));
+	vec.insert(vec.begin() + 7, ft::vector<int>(1, 1337));
+
+	vec.insert(vec.begin() + 1, 1, ft::vector<int>(1, 1337));
+	vec.insert(vec.begin() + 3, 3, ft::vector<int>(1, 1337));
+	vec.insert(vec.begin() + 5, 5, ft::vector<int>(1, 1337));
+	vec.insert(vec.begin() + 7, 7, ft::vector<int>(1, 1337));
+
+	vec.insert(vec.begin(), vec.begin(), vec.end());
 	vec.insert(vec.begin() + 3, vec.begin(), vec.end());
+	vec.insert(vec.begin() + 10, vec.begin(), vec.end());
+	vec.insert(vec.end() - 1, vec.begin(), vec.end());
+
 	
 	for (ft::vector<ft::vector<int> >::iterator it = vec.begin(); it != vec.end(); ++it)
-		std::cout << it->front() << "\t";
+		std::cout << it->back() << "\t";
 	std::cout << std::endl;
+
+	vec.erase(vec.begin());
+	vec.erase(vec.begin() + 1);
+	vec.erase(vec.begin() + 3);
+	vec.erase(vec.begin() + 5);
+	vec.erase(vec.begin() + 7);
+
+	vec.erase(vec.begin(), vec.begin() + 1);
+	vec.erase(vec.begin(), vec.begin() + 3);
+	vec.erase(vec.begin(), vec.begin() + 5);
+	vec.erase(vec.begin(), vec.begin() + 7);
 }
