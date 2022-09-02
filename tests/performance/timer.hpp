edf8fc0ed681a4931f057bbd25cc7023baa0f744
Ameer Nasr <anasr@lab1r1s6.42abudhabi.ae>
2022-09-02 11:34:39 +0400

    Add copy constructor and copy assignment operator for timer.hpp

--- tests/performance/timer.hpp
+++ tests/performance/timer.hpp
@@ -6,7 +6,7 @@
 /*   By: anasr <anasr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 17:44:09 by anasr             #+#    #+#             */
-/*   Updated: 2022/08/29 12:45:16 by anasr            ###   ########.fr       */
+/*   Updated: 2022/09/02 11:30:24 by anasr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,9 @@ class timer
 private:
 	clock_t start, end;
 public:
-
 	timer() {start = clock();}
+	timer(const timer& rhs) : start(rhs.start) {}
+	timer & operator=(const timer& rhs) {if (this != &rhs) {start = rhs.start;} return *this;}
 	~timer(){end = clock(); std::cout << "\e[32m" << (static_cast<double>(end) - static_cast<double>(start)) << " microseconds ( " << (static_cast<double>(end) - static_cast<double>(start)) / 1e6 << " seconds)\e[0m" << std::endl;}
 };
 
